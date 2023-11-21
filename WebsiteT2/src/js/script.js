6c9a5e65afec4d7d621d8afc320d01bec3783e7d
Riquelmi Santos <143042136+RiquelmiDev@users.noreply.github.com>
2023-11-21 00:38:15 -0300

    continuacao
    fiz o sobre e o portfolio

---
+++ WebsiteT2/src/js/script.js
@@ -0,0 +1,16 @@
+var iconmode = document.getElementById("iconmode");
+
+iconmode.onclick = function(){
+    document.body.classList.toggle("dark-theme");
+    if(document.body.classList.contains("dark-theme")){
+        iconmode.src = "src/img/moon.png";
+    }else{
+        iconmode.src = "src/img/sun.png";
+    }
+    /*troca de logo*/
+    if(document.body.classList.contains("dark-theme")){
+        imglogo.src = "src/img/RSlogoWf.png";
+    }else{
+        imglogo.src = "src/img/RSlogoBf.png";
+    }
+}
